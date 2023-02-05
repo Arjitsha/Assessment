9cef2a96d37ef7a38dfdb8a95c12e78a4ef9ab10
Arjitsha <102503715+Arjitsha@users.noreply.github.com>
2023-02-05 16:55:59 +0530

    Add files via upload

---
+++ arr.js
@@ -0,0 +1,17 @@
+import React from "react"
+const arr=[
+    {
+        id:1
+        data: (
+            <div>
+        <h>
+        <textarea
+        placeholder="Give Heading"
+        ></textarea>
+        
+        </h>
+        </div>
+        )
+        }
+    
+]
